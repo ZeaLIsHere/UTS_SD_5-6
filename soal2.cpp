b8e8622b0838f32b052c6a174b35dd0335aaa755
TariqRahmadari <tariq.rahmadari23@gmail.com>
2025-11-03 22:37:41 +0700

    Commit pertama soal2.cpp
    mengisi jawaban soal UTS nomor 2

--- soal2.cpp
+++ soal2.cpp
@@ -1,1 +1,49 @@
-//Masukkan jawaban disini
+#include <iostream>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    queue<string> antrianNormal;
+    queue<string> antrianDarurat;
+
+    int n;
+    cin >> n;
+
+    string cmd, nama;
+    for (int i = 0; i < n; ++i) {
+        cin >> cmd;
+
+        if (cmd == "DATANG") {
+            cin >> nama;
+            antrianNormal.push(nama);
+        } else if (cmd == "DARURAT") {
+            cin >> nama;
+            antrianDarurat.push(nama);
+        } else if (cmd == "LAYANI") {
+            if (!antrianDarurat.empty()) {
+                cout << antrianDarurat.front() << endl;
+                antrianDarurat.pop();
+            } else if (!antrianNormal.empty()) {
+                cout << antrianNormal.front() << endl;
+                antrianNormal.pop();
+            }
+        }
+    }
+    return 0;
+}
+
+
+//Penjelasan :
+ // Input:
+ //    * Program akan membaca jumlah (n).
+ //    * Looping sebanyak n kali untuk membaca setiap perintah.
+
+ // Logika Perintah:
+ //    DATANG [Nama]: Jika perintah adalah "DATANG", baca nama pasien, lalu masukkan (push) nama tersebut ke belakang `antrianNormal`.
+ //    DARURAT [Nama]: Jika perintah adalah "DARURAT", baca nama pasien, lalu masukkan (push) nama tersebut ke belakang `antrianDarurat`.
+ //    LAYANI: Jika perintah adalah "LAYANI":
+ //        - Cek terlebih dahulu `antrianDarurat`. Jika `!antrianDarurat.empty()` (tidak kosong), layani pasien darurat. Cetak nama pasien terdepan (`antrianDarurat.front()`) lalu keluarkan (pop) dari antrian darurat.
+ //        - Jika `antrianDarurat` kosong, baru cek `antrianNormal`. Jika `!antrianNormal.empty()` (tidak kosong), layani pasien normal. Cetak nama pasien terdepan (`antrianNormal.front()`) lalu keluarkan (pop) dari antrian normal.
+ //        - Jika kedua antrian kosong, "LAYANI" tidak melakukan apa-apa.
